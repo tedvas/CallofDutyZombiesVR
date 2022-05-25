98663e130e965accebe0c9546398a219173c3d8b
vster <vasanton2112@gmail.com>
2022-05-25 06:51:05 -0700

    Delete delete intermediate folders.bat

--- delete intermediate folders.bat
+++
@@ -1,6 +0,0 @@
-rmdir "C:\Unreal Engine Projects\CallOfDutyZombiesVR\Intermediate" /S /Q
-rmdir "C:\Unreal Engine Projects\CallOfDutyZombiesVR\Plugins\AdvancedSessions\Intermediate" /S /Q
-rmdir "C:\Unreal Engine Projects\CallOfDutyZombiesVR\Plugins\AdvancedSteamSessions\Intermediate" /S /Q
-rmdir "C:\Unreal Engine Projects\CallOfDutyZombiesVR\Plugins\OpenVRExpansionPlugin\Intermediate" /S /Q
-rmdir "C:\Unreal Engine Projects\CallOfDutyZombiesVR\Plugins\OpenXRExpansionPlugin\Intermediate" /S /Q
-rmdir "C:\Unreal Engine Projects\CallOfDutyZombiesVR\Plugins\VRExpansionPlugin\Intermediate" /S /Q
